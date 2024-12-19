ee99ed258ce7531a3675b224df02e1c43766b55a
Piyush <piushverma111@gmail.com>
2024-12-19 12:44:53 +0530

    First Commit

---
+++ MyLinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    internal class MyLinkedList
+    {
+        internal Node head;
+
+        public void InsertInFront(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+            }
+            node.next = head;
+            head = node;
+        }
+
+        public void InsertLast(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node last = GetLastNode();
+                last.next = node;
+            }
+        }
+
+        public Node GetLastNode()
+        {
+            Node temp = head;
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    internal class Node
+    {
+        internal int data;
+        internal Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}

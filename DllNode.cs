1219026c370ade364ae320e765ab95ffae13841f
Piyush <piushverma111@gmail.com>
2024-12-19 16:08:24 +0530

    Done with the Doubly Linked List

---
+++ DllNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    internal class DllNode
+    {
+        internal int data;
+        internal DllNode prev;
+        internal DllNode next;
+
+        public DllNode(int data)
+        {
+            this.data = data;
+            prev = null;
+            next = null;
+        }
+    }
+}
---
+++ DoubleLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+
+namespace DataStructures
+{
+    internal class DoubleLinkedList
+    {
+        internal DllNode head;
+
+        public void AddInFront(int data)
+        {
+            DllNode dll = new DllNode(data);
+            dll.next = head;
+            dll.prev = null;
+
+            if (head != null)
+            {
+                head.prev = dll;
+            }
+            head = dll;
+        }
+
+        public void AddInLast(int data)
+        {
+            DllNode dll = new DllNode(data);
+            if (head == null)
+            {
+                dll.prev = null;
+                head = dll;
+                return;
+            }
+            DllNode lastN = GetLastNode();
+            lastN.next = dll;
+            dll.prev = lastN;
+
+        }
+
+        public DllNode GetLastNode()
+        {
+            DllNode temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+
+        }
+    }
+}

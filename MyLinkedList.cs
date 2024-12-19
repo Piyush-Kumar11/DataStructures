1550fa3590420973b2c2111248802136aa5a4343
Piyush <piushverma111@gmail.com>
2024-12-19 15:34:47 +0530

    Added extra features to the LinkedList

--- MyLinkedList.cs
+++ MyLinkedList.cs
@@ -8,6 +8,80 @@ namespace DataStructures
     {
         internal Node head;
 
+        public bool SearchNode(int data)
+        {
+            Node temp = head;
+            while(temp!= null)
+            {
+                if(temp.data == data)
+                {
+                    return true;
+                }
+                temp = temp.next;
+            }
+            return false;
+        }
+
+        public void DisplayList()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + "=>");
+                temp = temp.next;
+            }
+            Console.WriteLine("null");
+        }
+
+        public int CountNode()
+        {
+            int count = 0;
+            Node temp = head;
+            while (temp != null)
+            {
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+
+        public void Reverse()
+        {
+            Node prev = null;
+            Node curr = head;
+            Node next = null;
+            while (curr != null)
+            {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            head = prev;
+        }
+
+        public void DeleteNode(int data)
+        {
+            Node temp = head;
+            if(head == null)
+            {
+                return;
+            }
+            if(head.data == data)
+            {
+                head = head.next;
+                return;
+            }
+            
+            while(temp.next != null && temp.next.data != data)
+            {
+                temp = temp.next;
+            }
+            if(temp.next != null)
+            {
+                temp.next = temp.next.next;
+            }
+        }
         public void InsertInFront(int data)
         {
             Node node = new Node(data);
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList ll = new MyLinkedList();
+
+            char c;
+            do
+            {
+                Console.WriteLine("Choose the Option:\n1.Add Elements\n2.Add Element in Front\n3.Search the Node\n4.Display List\n5.Count Nodes\n6.Reverse the List\n7.Delete a Node");
+                int i = Convert.ToInt32(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        Console.WriteLine("\nEnter the Data:");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        ll.InsertLast(n);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nEnter the Data:");
+                        int n1 = Convert.ToInt32(Console.ReadLine());
+                        ll.InsertInFront(n1);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nEnter the Element to find:");
+                        int n2 = Convert.ToInt32(Console.ReadLine());
+                        if (ll.SearchNode(n2))
+                            Console.WriteLine("Data is present!");
+                        else
+                            Console.WriteLine("Data Not Found!");
+                        break;
+
+                    case 4:
+                        ll.DisplayList();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("\nTotal Nodes are: " + ll.CountNode());
+                        break;
+
+                    case 6:
+                        ll.Reverse();
+                        ll.DisplayList();
+                        break;
+
+                    case 7:
+                        Console.WriteLine("\nEnter the Element to delete:");
+                        int n4 = Convert.ToInt32(Console.ReadLine());
+                        ll.DeleteNode(n4);
+                        Console.WriteLine("After Deleting: ");
+                        ll.DisplayList();
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter the correct option:");
+                        break;
+
+                }
+                //
+                Console.WriteLine("Enter Y/y to do any operation or other key to exit");
+                c = Convert.ToChar(Console.ReadLine());
+            } while (c == 'y' || c == 'Y');
+        }
+    }
+}
